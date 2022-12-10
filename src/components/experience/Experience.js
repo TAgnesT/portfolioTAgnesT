d0e8296491ff9339eae17b1678189d6f45c0327b
TAgnesT <105000830+TAgnesT@users.noreply.github.com>
2022-12-10 16:07:26 +0100

    update exp.js

--- src/components/experience/Experience.js
+++ src/components/experience/Experience.js
@@ -22,7 +22,7 @@ const Experience = () => {
 
             <article className='experience_details'><BsCheck2Circle className='experience_details-icon'/>
             <div>
-            <h4>CSS, SCSS, SAAS</h4>
+            <h4>CSS, SCSS, SASS</h4>
             <small className='text-light'>Experienced</small>
             </div>
             </article>
